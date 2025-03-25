1073b7ed8c6f3ae9d36ee08c57663f3a08b3b341
SikakolliMadhavika-github <140150457+SikakolliMadhavika@users.noreply.github.com>
2025-03-25 19:16:01 +0530

    Add files via upload

---
+++ 24-03/index.js
@@ -0,0 +1,78 @@
+let container = document.querySelector(".container");
+let url = "https://changeable-spice-plough.glitch.me/products" ;
+let dropdown = document.querySelector(".drop")
+
+async function getData(){
+    try {
+        let res = await fetch(url)
+        if( !res.ok){
+            throw new Error ("HTTP Error : ",res.statusText)
+        }
+        let product = await res.json()
+        localStorage.setItem("products", JSON.stringify(product));
+        let products = JSON.parse(localStorage.getItem('products'))
+        displayData(products)
+        displaydropdown(products)
+    } catch (err) {
+        console.error(err)     
+    }
+}
+
+function displayData(products){
+    container.innerHTML = '';
+    products.forEach(obj => {
+        let item = document.createElement('div');
+        item.className = 'item'
+        item.innerHTML = `
+        <img src = '${obj.image}'>
+        <p> <b>Title : </b>${obj.title} </p>
+        <p><b> Price :</b> ${obj.price}</p>
+        <p><i><b> Category : ${obj.category} </b></i><br>
+        <button onclick = deleteData('${obj.id}')>Delete</button> 
+        `;
+        container.appendChild(item);
+    });
+}
+
+async function deleteData(id){
+    try {
+        let res = await fetch(`${url}/${id}`, {
+            "method" : "DELETE"
+        })
+        if (!res.ok){
+            throw new Error ("HTTP Error :", res.status)
+        }
+        if(res.ok){
+            getData();
+            alert("Data deleted succesfully");
+        } 
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+function displaydropdown(products){
+    dropdown.innerHTML = '';
+    let categoryArr = products.map(obj => obj.category)
+    let defaultOpt = document.createElement("option")
+    defaultOpt.textContent = "Select Category"
+    defaultOpt.disabled = true
+    defaultOpt.selected = true
+    dropdown.appendChild(defaultOpt)
+
+    Array.from(new Set(categoryArr)).forEach( category => {
+        let option = document.createElement("option")
+        option.innerHTML = category
+        option.value = category
+        dropdown.appendChild(option)
+    })
+}
+
+dropdown.addEventListener("change", (cate) => filterData(cate.target.value));
+function filterData(category){
+    let products = JSON.parse(localStorage.getItem("products"));
+    let result = products.filter(obj => obj.category == category)
+    displayData(result)
+}
+
+getData()
