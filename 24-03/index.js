8ea46fc754d99e95d1164706e0781538507fb1c9
SikakolliMadhavika-github <140150457+SikakolliMadhavika@users.noreply.github.com>
2025-03-25 21:14:36 +0530

    Add files via upload

--- 24-03/index.js
+++ 24-03/index.js
@@ -20,18 +20,25 @@ async function getData(){
 
 function displayData(products){
     container.innerHTML = '';
-    products.forEach(obj => {
-        let item = document.createElement('div');
-        item.className = 'item'
-        item.innerHTML = `
-        <img src = '${obj.image}'>
-        <p> <b>Title : </b>${obj.title} </p>
-        <p><b> Price :</b> ${obj.price}</p>
-        <p><i><b> Category : ${obj.category} </b></i><br>
-        <button onclick = deleteData('${obj.id}')>Delete</button> 
-        `;
-        container.appendChild(item);
-    });
+    if(products.length == 0){
+        container.innerHTML = `
+        <h1> No Data available </h1>
+        `
+    }
+    else{
+        products.forEach(obj => {
+            let item = document.createElement('div');
+            item.className = 'item'
+            item.innerHTML = `
+            <img src = '${obj.image}'>
+            <p> <b>Title : </b>${obj.title} </p>
+            <p><b> Price :</b> ${obj.price}</p>
+            <p><i><b> Category : ${obj.category} </b></i><br>
+            <button onclick = deleteData('${obj.id}')>Delete</button> 
+            `;
+            container.appendChild(item);
+        });
+    }
 }
 
 async function deleteData(id){
@@ -53,19 +60,21 @@ async function deleteData(id){
 
 function displaydropdown(products){
     dropdown.innerHTML = '';
-    let categoryArr = products.map(obj => obj.category)
-    let defaultOpt = document.createElement("option")
-    defaultOpt.textContent = "Select Category"
-    defaultOpt.disabled = true
-    defaultOpt.selected = true
-    dropdown.appendChild(defaultOpt)
+    if(products.length != 0){
+        let categoryArr = products.map(obj => obj.category)
+        let defaultOpt = document.createElement("option")
+        defaultOpt.textContent = "Select Category"
+        defaultOpt.disabled = true
+        defaultOpt.selected = true
+        dropdown.appendChild(defaultOpt)
 
-    Array.from(new Set(categoryArr)).forEach( category => {
-        let option = document.createElement("option")
-        option.innerHTML = category
-        option.value = category
-        dropdown.appendChild(option)
-    })
+        Array.from(new Set(categoryArr)).forEach( category => {
+            let option = document.createElement("option")
+            option.innerHTML = category
+            option.value = category
+            dropdown.appendChild(option)
+        })
+    }    
 }
 
 dropdown.addEventListener("change", (cate) => filterData(cate.target.value));
